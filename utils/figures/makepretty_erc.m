f22c83ea8723cd988b9467c19e5122acdbe103ac
samlaventure <samuel.laventure@gmail.com>
2020-12-16 20:19:55 +0100

    added possibility to save multiple type of format simultaneously

--- utils/figures/makepretty_erc.m
+++ utils/figures/makepretty_erc.m
@@ -5,6 +5,7 @@ function makepretty_erc(varargin)
 %
 % INPUTS:
 %       - fsize             Font size for X and Y labels (default is 17)
+%       - fsizet            font size for title (default is 16)
 %       - lwidth            Line width (default is 2)
 %
 % NOTES:
@@ -18,44 +19,47 @@ for i = 1:2:length(varargin)
 		error(['Parameter ' num2str(i+2) ' is not a property (type ''help <a href="matlab:help malepretty_erc">makepretty_erc</a>'' for details).']);
 	end
 	switch(lower(varargin{i}))
-        case 'fsize'
-            fsize =  varargin{i+1};
-            if ~isnumeric(fsize)
-				error('Incorrect value for property ''fsize''');
+        case 'fsizel'
+            fsizel =  varargin{i+1};
+            if ~isnumeric(fsizel)
+				error('Incorrect value for property ''fsizel''');
             end
+         case 'fsizet'
+			fsizet = varargin{i+1};
+			if ~isnumeric(fsizet)
+				error('Incorrect value for property ''fsizet'' (type ''help <a href="matlab:help FindSpindlesSB">FindSpindlesSB</a>'' for details).');
+            end  
 		case 'lwidth'
 			lwidth = varargin{i+1};
 			if ~isnumeric(lwidth)
 				error('Incorrect value for property ''lwidth'' (type ''help <a href="matlab:help FindSpindlesSB">FindSpindlesSB</a>'' for details).');
-            end
+            end  
     end    
 end
 %Default values 
-if ~exist('fsize','var')
-    fsize = 17;
+if ~exist('fsizel','var')
+    fsizel = 17;
+end
+if ~exist('fsizet','var')
+    fsizet = 16;
 end
 if ~exist('lwidth','var')
     lwidth = 2;
 end
 
 % set some graphical attributes of the current axis
-set(get(gca, 'XLabel'), 'FontSize', fsize);
-set(get(gca, 'YLabel'), 'FontSize', fsize);
+set(get(gca, 'XLabel'), 'FontSize', fsizel);
+set(get(gca, 'YLabel'), 'FontSize', fsizel);
 set(gca, 'FontSize', 13);
 box off
-set(gca,'Linewidth',1.6)
-set(get(gca, 'Title'), 'FontSize', 16);
+set(gca,'Linewidth',lwidth)
+set(get(gca, 'Title'), 'FontSize', fsizet);
 
 ch = get(gca, 'Children');
 
 for c = 1:length(ch)
     thisChild = ch(c);
     if strcmp('line', get(thisChild, 'Type'))
-        if strcmp('.', get(thisChild, 'Marker'))
-            % if get(thisChild, 'MarkerSize')<15
-            %             set(thisChild, 'MarkerSize', 15);
-            % end
-        end
         if strcmp('-', get(thisChild, 'LineStyle'))
             set(thisChild, 'LineWidth', lwidth);
         end

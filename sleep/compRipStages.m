a408ab6e4cb7334e19d3a10f58730804f6de7569
samlaventure <samuel.laventure@gmail.com>
2020-12-16 20:42:06 +0100

    Multiple changes: added makepretty_erc and deleted parts related to this. Deleted save. Fixed legend.

--- sleep/compRipStages.m
+++ sleep/compRipStages.m
@@ -20,9 +20,6 @@ function [figH] = compRipStages(expe, mice_num)
 %==========================================================================
 
 %% Parameters
-sav=1;
-
-
 if strcmp(expe,'StimMFBWake')
     Dir = PathForExperimentsERC_SL(expe);
 elseif strcmp(expe,'UMazePAG') 
@@ -33,12 +30,12 @@ else
 end
 Dir = RestrictPathForExperiment(Dir, 'nMice', mice_num);
 
-% set text format
-set(0,'defaulttextinterpreter','latex');
-set(0,'DefaultTextFontname', 'Arial')
-set(0,'DefaultAxesFontName', 'Arial')
-set(0,'defaultTextFontSize',12)
-set(0,'defaultAxesFontSize',12)
+% % set text format
+% set(0,'defaulttextinterpreter','latex');
+% set(0,'DefaultTextFontname', 'Arial')
+% set(0,'DefaultAxesFontName', 'Arial')
+% set(0,'defaultTextFontSize',12)
+% set(0,'defaultAxesFontSize',12)
 
 % var init
 stageName = {'NREM','Wake','N1','N2','N3','Sleep'};
@@ -54,7 +51,7 @@ for isuj = 1:length(Dir.path)
     [sEpoch{isuj} subst(isuj)] = get_SleepEpoch(Dir.path{isuj}{1},Vtsd);
 end
 % get sleep event details
-[rip ripmean] = get_SleepEvent(Dir.path,'Ripples',sEpoch,subst);
+[rip ripmean ripdif] = get_SleepEvent(Dir.path,'Ripples',sEpoch,subst);
            
 % count number of rip by subjet and stage
 if length(Dir.path)==1
@@ -70,7 +67,7 @@ end
 %#                        F I G U R E S
 %#####################################################################
 
-%% FIGURE RIPPLES
+%% FIGURE RIPPLES MAIN
 
 % prep data for figures
 % get rid of empty data (without substaging)
@@ -92,7 +89,7 @@ end
 % prep var - amplitude waveforms
 maxy = max(max(max(max(squeeze(squeeze(squeeze(ripmean.waveforms(:,:,:,:))))))))*1.2;
 miny = min(min(min(min(squeeze(squeeze(squeeze(ripmean.waveforms(:,:,:,:))))))))*1.2;
-supertit = 'Ripples during pre/post sleep - global figure';
+supertit = 'Ripples during pre/post sleep - global';
 figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'Name', supertit, 'NumberTitle','off');
     % Set plot titles
     ypos = 1.03; % position with 6 x 7 figure with 1600x2200 pixels (rate of diff between plot is .14)
@@ -100,6 +97,18 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
         a1 = annotation('textbox',[.05 ypos-.14*iplot 0 0],...
             'String',ptitles{iplot},'FitBoxToText','on','EdgeColor','none'); 
         a1.FontSize = 12;
+        if iplot==1
+            % creating legend with hidden-fake data for waveforms plots
+                s1 = subplot(6,6,1);
+                    axP = get(gca,'Position');
+                    p1=plot(nan(2,1),'-k','LineWidth',2);
+                    hold on
+                    p2=plot(nan(3,1),'-b','LineWidth',2);
+                    hold off
+                    legend([p1 p2],{'pre','post'},'location','East');
+                    set(gca, 'Position', axP)
+                    set(s1,'Visible','off')
+        end
     end
     % Set Number of mouse per stage at bottom of figure
     xpos = 0.184;
@@ -166,15 +175,10 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
                 title([stageName{istage}])
                 xlabel('time (ms)')
                 ylim([miny maxy])
-                xticks([0 30 60 90 120 150])
+                xticks([0 75 150])
+                makepretty_erc('fsizel',10,'lwidth',1.5,'fsizet',16)
                 if istage==1
                     ylabel('uV')
-                    %legend (make legend outside of plot and subplot!)
-                    axP = get(gca,'Position');
-                    [~,hobj,~,~] = legend([hpre.mainLine hpost.mainLine],{'pre','post'},'Location','westoutside');
-                    hl = findobj(hobj,'type','line');
-                    set(hl,'LineWidth',3); % make line ticker
-                    set(gca, 'Position', axP)
                 end
         end
     end
@@ -199,6 +203,8 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
         set(h, 'LineWidth', 1);
         set(her, 'LineWidth', 1);
         ylabel('uV');
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
+        
         % creating legend with hidden-fake data (hugly but effective)
         axP = get(gca,'Position');
         b2=bar([-2],[ 1],'FaceColor','flat');
@@ -225,6 +231,7 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
         set(h, 'LineWidth', 1);
         set(her, 'LineWidth', 1);
         ylabel('Hz');
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
         
     durdat = [squeeze(ripmean.dur(1,1,:)) squeeze(ripmean.dur(2,1,:)) nanfix squeeze(ripmean.dur(1,2,:)) squeeze(ripmean.dur(2,2,:)) nanfix ...
              squeeze(ripmean.dur(1,3,:)) squeeze(ripmean.dur(2,3,:)) nanfix squeeze(ripmean.dur(1,4,:)) squeeze(ripmean.dur(2,4,:)) nanfix ...
@@ -243,7 +250,8 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
         set(h, 'LineWidth', 1);
         set(her, 'LineWidth', 1);
         ylabel('ms');
-
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
+        
      globaldendat = [squeeze(ripmean.globalden(1,1,:)) squeeze(ripmean.globalden(2,1,:)) nanfix squeeze(ripmean.globalden(1,2,:)) squeeze(ripmean.globalden(2,2,:)) nanfix ...
          squeeze(ripmean.globalden(1,3,:)) squeeze(ripmean.globalden(2,3,:)) nanfix squeeze(ripmean.globalden(1,4,:)) squeeze(ripmean.globalden(2,4,:)) nanfix ...
          squeeze(ripmean.globalden(1,5,:)) squeeze(ripmean.globalden(2,5,:))];
@@ -261,7 +269,8 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
         set(h, 'LineWidth', 1);
         set(her, 'LineWidth', 1);
         ylabel('event/sec');
- 
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
+        
     localdendat = [squeeze(ripmean.localden(1,1,:)) squeeze(ripmean.localden(2,1,:)) nanfix squeeze(ripmean.localden(1,2,:)) squeeze(ripmean.localden(2,2,:)) nanfix ...
          squeeze(ripmean.localden(1,3,:)) squeeze(ripmean.localden(2,3,:)) nanfix squeeze(ripmean.localden(1,4,:)) squeeze(ripmean.localden(2,4,:)) nanfix ...
          squeeze(ripmean.localden(1,5,:)) squeeze(ripmean.localden(2,5,:))];
@@ -279,15 +288,132 @@ figH.global = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1600 2200],'N
         set(h, 'LineWidth', 1);
         set(her, 'LineWidth', 1);
         ylabel('event/min');
-if sav
-    % Directory to save and name of the figure to save
-    dir_out = [dropbox '/DataSL/StimMFBWake/ripples/Sleep/' date '/'];
-    %create folders
-    if ~exist(dir_out,'dir')
-        mkdir(dir_out);
-    end    
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
 
-    % save figure
-    print([dir_out 'GlobalAnalyses_ripples_' num2str(mice_num)], '-dpng', '-r300');
-end
+
+%% FIGURE RIPPLES DIFFERENCES
+
+% prep plot titles
+ptitles = {'Peak Amplitude','Frequency','Duration','Global Density','Local Density'};
+
+supertit = 'Ripples during pre/post sleep - differences';
+figH.diff = figure('Color',[1 1 1], 'rend','painters','pos',[1 1 1100 800],'Name', supertit, 'NumberTitle','off');
+    % Set plot titles
+    ypos = 1.05; 
+    for iplot=1:5
+        a1 = annotation('textbox',[.05 ypos-.17*iplot 0 0],...
+            'String',ptitles{iplot},'FitBoxToText','on','EdgeColor','none'); 
+        a1.FontSize = 12;
+    end
+       
+    % amplitude
+    subplot(5,6,2:6)
+       [p,h,her] = PlotErrorBarN_SL(ripdif.amp(1:5,:)',...
+                'barwidth', 0.6, 'newfig', 0,'barcolors',[.3 .3 .3]);
+        h.FaceColor = 'flat';
+        h.CData(1,:) = [0 0 0]; 
+        h.CData(2,:) = [1 1 1];
+        h.CData(3,:) = [.8 .8 .8]; 
+        h.CData(4,:) = [.5 .5 .5]; 
+        h.CData(5,:) = [.2 .2 .2];
+        set(gca,'xticklabel',{[]})    
+        ylabel({'% change','in uV'});
+        ymin = min(min(ripdif.amp(1:5,:)));
+        ymax = max(max(ripdif.amp(1:5,:)));
+        if sign(ymin)>0, ymin=0; else ymin=ymin*1.15; end
+        if sign(ymax)>0, ymax=ymax*1.15; else ymax=0; end    
+        ylim([ymin ymax])
+        makepretty_erc('fsizel',10,'lwidth',1.5,'fsizet',16)
         
+        % creating legend with hidden-fake data (hugly but effective)
+        axP = get(gca,'Position');
+        b1=bar([-3],[ 1],'FaceColor','flat');
+        b2=bar([-2],[ 1],'FaceColor','flat');
+        b3=bar([-3],[ 1],'FaceColor','flat');
+        b4=bar([-3],[ 1],'FaceColor','flat');
+        b5=bar([-3],[ 1],'FaceColor','flat');
+        b1.CData(1,:) = repmat([0 0 0],1);
+        b2.CData(1,:) = repmat([1 1 1],1);
+        b3.CData(1,:) = repmat([.8 .8 .8],1);
+        b4.CData(1,:) = repmat([.5 .5 .5],1);
+        b5.CData(1,:) = repmat([.2 .2 .2],1);
+        legend([b1 b2 b3 b4 b5],stageName,'Location','EastOutside')
+        set(gca, 'Position', axP)
+    
+    % Freq
+    subplot(5,6,8:12)
+       [p,h,her] = PlotErrorBarN_SL(ripdif.freq(1:5,:)',...
+                'barwidth', 0.6, 'newfig', 0,'barcolors',[.3 .3 .3]);
+        h.FaceColor = 'flat';
+        h.CData(1,:) = [0 0 0]; 
+        h.CData(2,:) = [1 1 1];
+        h.CData(3,:) = [.8 .8 .8]; 
+        h.CData(4,:) = [.5 .5 .5]; 
+        h.CData(5,:) = [.2 .2 .2];
+        set(gca,'xticklabel',{[]})    
+        ylabel({'% change','in Hz'});
+        ymin = min(min(ripdif.freq(1:5,:)));
+        ymax = max(max(ripdif.freq(1:5,:)));
+        if sign(ymin)>0, ymin=0; else ymin=ymin*1.15; end
+        if sign(ymax)>0, ymax=ymax*1.15; else ymax=0; end    
+        ylim([ymin ymax])
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
+        
+    % Dur
+    subplot(5,6,14:18)
+       [p,h,her] = PlotErrorBarN_SL(ripdif.dur(1:5,:)',...
+                'barwidth', 0.6, 'newfig', 0,'barcolors',[.3 .3 .3]);
+        h.FaceColor = 'flat';
+        h.CData(1,:) = [0 0 0]; 
+        h.CData(2,:) = [1 1 1];
+        h.CData(3,:) = [.8 .8 .8]; 
+        h.CData(4,:) = [.5 .5 .5]; 
+        h.CData(5,:) = [.2 .2 .2];
+        set(gca,'xticklabel',{[]})    
+        ylabel({'% change','in ms'});
+        ymin = min(min(ripdif.dur(1:5,:)));
+        ymax = max(max(ripdif.dur(1:5,:)));
+        if sign(ymin)>0, ymin=0; else ymin=ymin*1.15; end
+        if sign(ymax)>0, ymax=ymax*1.15; else ymax=0; end    
+        ylim([ymin ymax])
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
+        
+    % Global Density
+    subplot(5,6,20:24)
+       [p,h,her] = PlotErrorBarN_SL(ripdif.globalden(1:5,:)',...
+                'barwidth', 0.6, 'newfig', 0,'barcolors',[.3 .3 .3]);
+        h.FaceColor = 'flat';
+        h.CData(1,:) = [0 0 0]; 
+        h.CData(2,:) = [1 1 1];
+        h.CData(3,:) = [.8 .8 .8]; 
+        h.CData(4,:) = [.5 .5 .5]; 
+        h.CData(5,:) = [.2 .2 .2];
+        set(gca,'xticklabel',{[]})    
+        ylabel({'% change','in rip/sec'});
+        ymin = min(min(ripdif.globalden(1:5,:)));
+        ymax = max(max(ripdif.globalden(1:5,:)));
+        if sign(ymin)>0, ymin=0; else ymin=ymin*1.15; end
+        if sign(ymax)>0, ymax=ymax*1.15; else ymax=0; end    
+        ylim([ymin ymax])
+        makepretty_erc('fsizel',12,'lwidth',1.5,'fsizet',16)
+        
+    % Local Density
+    subplot(5,6,26:30)
+       [p,h,her] = PlotErrorBarN_SL(ripdif.localden(1:5,:)',...
+                'barwidth', 0.6, 'newfig', 0,'barcolors',[.3 .3 .3]);
+        h.FaceColor = 'flat';
+        h.CData(1,:) = [0 0 0]; 
+        h.CData(2,:) = [1 1 1];
+        h.CData(3,:) = [.8 .8 .8]; 
+        h.CData(4,:) = [.5 .5 .5]; 
+        h.CData(5,:) = [.2 .2 .2];
+        set(gca,'xticklabel',{[]})    
+        ylabel({'% change','in rip/min'});
+        set(gca,'xtick',[1:5],'xticklabel',stageName) 
+        ymin = min(min(ripdif.localden(1:5,:)));
+        ymax = max(max(ripdif.localden(1:5,:)));
+        if sign(ymin)>0, ymin=0; else ymin=ymin*1.15; end
+        if sign(ymax)>0, ymax=ymax*1.15; else ymax=0; end    
+        ylim([ymin ymax])
+        makepretty_erc('fsizel',10,'lwidth',1.5,'fsizet',16)
+end

bed91e43f8f26263711f2caaae249f9cec857b32
samlaventure <samuel.laventure@gmail.com>
2020-12-26 19:28:21 +0100

    minor aesthetic changes

--- sleep/sam_sleep_prevspost.m
+++ sleep/sam_sleep_prevspost.m
@@ -50,7 +50,7 @@ end
 
 %% Parameters
 % Get directories
-if strcmp(expe,'StimMFBWake')
+if strcmp(expe,'StimMFBWake') || strcmp(expe,'Novel')
     Dir = PathForExperimentsERC_SL(expe);
 elseif strcmp(expe,'UMazePAG') 
     Dir = PathForExperimentsERC_Dima(expe);

ee26eb2d7922af73f372a6f900a8f07fc4a47c2f
samlaventure <samuel.laventure@gmail.com>
2021-02-27 22:30:38 +0100

    New get event function for pre- post-sleep sessions

---
+++ sleep/SleepEventsFx/get_SleepEpoch_sess.m
@@ -0,0 +1,81 @@
+function [sEpoch subst] = get_SleepEpoch_sess(dirPath,Vtsd)
+
+%==========================================================================
+% Details: get sleep stages epochs for SleepEvents analyses
+%
+% INPUTS:
+%       - Vtsd              Vtsd from behavResources (speed in tsd format)
+%
+% OUTPUT:
+%       - sEpoch            Epoch (timestamps) for each sleep/wake stages
+%       - subst             Substages done (yes=1; no=0)
+%
+% NOTES:
+%
+%   Written by Samuel Laventure - 2020-11/12
+%      
+%==========================================================================
+
+%% GET SESSION and STAGE EPOCHS
+% load sleep scoring
+try
+    load([dirPath 'SleepSubstages.mat'],'Epoch','Epoch_sess');
+    if exist('Epoch_sess','var')
+        Epoch = Epoch_sess;
+        sEpoch{3,1} = or(Epoch{1}{7},Epoch{2}{7});        % nrem
+        sEpoch{3,2} = or(Epoch{1}{4},Epoch{2}{4});        % rem
+        sEpoch{3,3} = or(Epoch{1}{5},Epoch{2}{5});        % wake
+        sEpoch{3,4} = or(Epoch{1}{1},Epoch{2}{1});        % n1
+        sEpoch{3,5} = or(Epoch{1}{2},Epoch{2}{2});        % n2
+        sEpoch{3,6} = or(Epoch{1}{3},Epoch{2}{3});        % n3
+        sEpoch{3,7} = or(or(Epoch{1}{4},Epoch{1}{7}), ...
+                        or(Epoch{2}{4},Epoch{2}{7}));      % sleep  
+    else
+        sEpoch{3,1} = Epoch{7};                   % nrem
+        sEpoch{3,2} = Epoch{4};                   % rem
+        sEpoch{3,3} = Epoch{5};                   % wake
+        sEpoch{3,4} = Epoch{1};                   % n1
+        sEpoch{3,5} = Epoch{2};                   % n2
+        sEpoch{3,6} = Epoch{3};                   % n3
+        sEpoch{3,7} = or(Epoch{5},Epoch{7});      % sleep
+    end
+    subst = 1;
+    clear Epoch
+    try
+        load([dirPath 'SleepScoring_OBGamma.mat'],'sleep_array');
+    catch
+        load([dirPath 'SleepScoring_Accelero.mat'],'sleep_array');
+    end 
+catch
+    try
+        load([dirPath 'SleepScoring_OBGamma.mat'], 'Wake', 'Sleep', 'SWSEpoch', 'REMEpoch');
+    catch
+        load([dirPath 'SleepScoring_Accelero.mat'], 'Wake', 'Sleep', 'SWSEpoch', 'REMEpoch');
+    end 
+    sEpoch{3,1} = SWSEpoch;                   % nrem
+    sEpoch{3,2} = REMEpoch;                   % rem
+    sEpoch{3,3} = Wake;                       % wake
+    sEpoch{3,7} = Sleep;                      % sleep
+    subst = 0;
+    clear Wake Sleep SWSEpoch REMEpoch
+end
+
+% load behavResources (session epochs)
+load([dirPath 'behavResources.mat'], 'SessionEpoch','Vtsd');
+% restrict to pre/post sessions
+for istage=1:7
+    if ~(subst) && istage>3 
+        istage = 7;
+    end
+    % speed restriction
+    % Locomotion threshold
+    immobileEpoch = thresholdIntervals(tsd(Range(Vtsd)...
+        ,movmedian(Data(Vtsd),5)),5,'Direction','Below');
+    try
+        sEpoch{1,istage} = and(and(sEpoch{3,istage},SessionEpoch.BaselineSleep),immobileEpoch);    
+    catch
+        sEpoch{1,istage} = and(and(sEpoch{3,istage},SessionEpoch.PreSleep),immobileEpoch);    
+    end
+    sEpoch{2,istage} = and(and(sEpoch{3,istage},SessionEpoch.PostSleep),immobileEpoch);    
+end
+end
